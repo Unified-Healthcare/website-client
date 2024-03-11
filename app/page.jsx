b380cc46e7c6433bf489a64e744ec88aeff60c12
Lavan JV <70334120+jvlavan@users.noreply.github.com>
2024-03-11 19:47:12 +0530

    initial-v5

--- app/page.jsx
+++ app/page.jsx
@@ -7,17 +7,18 @@ import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
 import React, { useState } from 'react';
-import { QrReader } from 'react-qr-reader';
-
+import  Html5QrcodePlugin  from '@/components/Html5QrcodePlugin';
 
 export default function Home() {
-	const [data, setData] = useState('No result');
-	const [facingMode, setFacingMode] = useState('environment');
-	const toggleFacingMode = () => {
-		setFacingMode((prevFacingMode) =>
-		  prevFacingMode === 'environment' ? 'user' : 'environment'
-		);
-	  };
+
+	const [decodedText, setDecodedText] = useState('No result');
+	const [decodedResult, setDecodedResult] = useState('No result');
+	const onNewScanResult = (decodedText, decodedResult) => {
+        // handle decoded results here
+		setDecodedResult(decodedResult);
+		setDecodedText(decodedText);
+
+    };
 	return (<>
 		<section className="bg-white dark:bg-gray-900">
     <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
@@ -26,26 +27,17 @@ export default function Home() {
             <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Please allow camera access permission and wait some time for reading the QR code generated</p>
      
         </div>
-        <div className=" lg:mt-0 lg:col-span-5 ">
-		<QrReader
-		facingMode={facingMode}
-        onResult={(result, error) => {
-          if (!!result) {
-            setData(result?.text);
-          }
-
-          if (!!error) {
-            console.info(error);
-          }
-        }}
-        style={{ width: '100%' }}
-      />
-      <button onClick={toggleFacingMode}>
-        Switch Camera ({facingMode === 'environment' ? 'Front' : 'Back'})
-      </button>
-        </div>   
-		<br />         
-		<p>{data}</p>
+        <div className=" lg:mt-0 lg:col-span-5 text-black bg-white">
+		<Html5QrcodePlugin
+                fps={10}
+                qrbox={250}
+                disableFlip={false}
+                qrCodeSuccessCallback={onNewScanResult}
+            />
+      
+        </div>            
+		<p>{JSON.stringify(decodedText)}</p>
+		<p>{JSON.stringify(decodedResult)}</p>
     </div>
 </section>
 		
