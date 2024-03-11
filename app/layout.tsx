0f9d1407a969b7c994fc229af8a690d74842e709
Lavan JV <70334120+jvlavan@users.noreply.github.com>
2024-03-11 19:12:55 +0530

    initial

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,13 +46,13 @@ export default function RootLayout({
 						</main>
 						<footer className="w-full flex items-center justify-center py-3">
 							<Link
-								isExternal
+							
 								className="flex items-center gap-1 text-current"
-								href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
-								title="nextui.org homepage"
+								href="/kalndevs"
+								title="Credits"
 							>
 								<span className="text-default-600">Powered by</span>
-								<p className="text-primary">NextUI</p>
+								<p className="text-primary">Kaln Devs</p>
 							</Link>
 						</footer>
 					</div>
---
+++ app/page.jsx
@@ -0,0 +1,44 @@
+"use client"
+import { Link } from "@nextui-org/link";
+import { Snippet } from "@nextui-org/snippet";
+import { Code } from "@nextui-org/code"
+import { button as buttonStyles } from "@nextui-org/theme";
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+import React, { useState } from 'react';
+import { QrReader } from 'react-qr-reader';
+
+
+export default function Home() {
+	const [data, setData] = useState('No result');
+	return (<>
+		<section className="bg-white dark:bg-gray-900">
+    <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div className="mr-auto place-self-center lg:col-span-7">
+            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Unified HealthCare QR Scanner</h1>
+            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Please allow camera access permission and wait some time for reading the QR code generated</p>
+     
+        </div>
+        <div className=" lg:mt-0 lg:col-span-5 ">
+		<QrReader
+        onResult={(result, error) => {
+          if (!!result) {
+            setData(result?.text);
+          }
+
+          if (!!error) {
+            console.info(error);
+          }
+        }}
+        style={{ width: '100%' }}
+      />
+      
+        </div>            
+		<p>{data}</p>
+    </div>
+</section>
+		
+		</>
+	);
+}
